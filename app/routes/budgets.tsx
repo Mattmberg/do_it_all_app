c4014983d54fd142467c31bc264786e6e538b8a8
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2022-06-10 21:33:24 -0400

    added_stylesheet_link

--- app/routes/budgets.tsx
+++ app/routes/budgets.tsx
@@ -1,3 +1,13 @@
+import type {
+  LinksFunction,
+} from "@remix-run/node";
+
+import stylesUrl from "~/styles/budgets.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl, }];
+};
+
 export default function Index() {
     return (
       <div className="container">
