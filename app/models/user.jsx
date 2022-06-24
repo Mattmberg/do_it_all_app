ac773bcb0607eee9ebdd593976df0963a45c8e93
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2022-06-23 21:40:29 -0400

    prisma_and_more

---
+++ app/models/user.jsx
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    } 
+});
+
+export const User = mongoose.model("User", UserSchema);
