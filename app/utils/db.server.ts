dcdcddfcf8887a1606edff0d58cdceedfe6f8ea0
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2022-07-21 19:23:41 -0400

    prisma_fix

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -6,13 +6,9 @@ declare global {
   var __db: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient();
-} else {
-  if (!global.__db) {
-    global.__db = new PrismaClient();
-  }
-  db = global.__db;
+if (!global.__db) {
+  global.__db = new PrismaClient();
 }
+db = global.__db;
 
 export { db };
