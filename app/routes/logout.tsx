8ef4d946b1b5c334bfd655d844597d4518f07c35
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2022-07-21 19:14:13 -0400

    rolling_out_login

---
+++ app/routes/logout.tsx
@@ -0,0 +1,17 @@
+import type {
+    ActionFunction,
+    LoaderFunction,
+  } from "@remix-run/cloudflare";
+  import { redirect } from "@remix-run/cloudflare";
+  
+  import { logout } from "~/utils/session.server";
+  
+  export const action: ActionFunction = async ({
+    request,
+  }) => {
+    return logout(request);
+  };
+  
+  export const loader: LoaderFunction = async () => {
+    return redirect("/");
+  };
