774fc628a6ee9720c50218c4eb6ccef4da446101
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2022-07-22 17:19:53 -0400

    removed_root_header

--- app/root.tsx
+++ app/root.tsx
@@ -122,23 +122,6 @@ function Document({
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="remix-app">
-      <header className="remix-app__header">
-        <div className="container remix-app__header-content">
-          <nav aria-label="Main navigation" className="remix-app__header-nav">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/matthew-bergeron-jr-cs/">LinkedIn</a>
-              </li>
-              <li>
-                <a href="https://github.com/Mattmberg">GitHub</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
       <div className="remix-app__main">
         <div className="container remix-app__main-content">{children}</div>
       </div>
