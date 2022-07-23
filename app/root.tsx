c314045327ac516d12f6cd8ad14ef880d9e7b4fb
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2022-07-22 20:41:40 -0400

    bg_fix

--- app/root.tsx
+++ app/root.tsx
@@ -8,22 +8,6 @@ import {
   ScrollRestoration,
   useCatch
 } from "remix";
-import type { LinksFunction } from "remix";
-
-import globalStylesUrl from "~/styles/global.css";
-import darkStylesUrl from "~/styles/dark.css";
-
-// https://remix.run/api/app#links
-export let links: LinksFunction = () => {
-  return [
-    { rel: "stylesheet", href: globalStylesUrl },
-    {
-      rel: "stylesheet",
-      href: darkStylesUrl,
-      media: "(prefers-color-scheme: dark)"
-    }
-  ];
-};
 
 // https://remix.run/api/conventions#default-export
 // https://remix.run/api/conventions#route-filenames
