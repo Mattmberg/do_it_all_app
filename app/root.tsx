bc64406f0b104e58c6f3638700a05dd297005917
Matthew Bergeron Jr <Matthewmckinley16@gmail.com>
2022-08-12 08:12:36 -0400

    removed_root_container

--- app/root.tsx
+++ app/root.tsx
@@ -114,7 +114,7 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="remix-app">
       <div className="remix-app__main">
-        <div className="container remix-app__main-content">{children}</div>
+        <div className="remix-app__main-content">{children}</div>
       </div>
     </div>
   );
