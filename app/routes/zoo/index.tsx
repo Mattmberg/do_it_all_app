319b65e87bbf607cfa7b944a184b5f9bf7236905
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2022-09-05 14:19:53 -0400

    Update index.tsx

--- app/routes/zoo/index.tsx
+++ app/routes/zoo/index.tsx
@@ -1,1 +1,26 @@
+import type {
+    LinksFunction,
+} from "@remix-run/cloudflare";
+import { Link } from "@remix-run/react";
+import stylesUrl from "~/styles/onlinestore.css";
+
+export const links: LinksFunction = () => {
+    return [{ rel: "stylesheet", href: stylesUrl, }];
+};
+
+export default function Index() {
+    return (
+      <div className="container">
+        <header>
+        </header>
+        <div className="content">
+        </div>
+        <footer>
+            <div className="footerLinks">
+                <p>Facebook Twitter Instagram Icon Spots - Everything on this page is demo only.</p>
+            </div>
+        </footer>
+      </div>
+      );
+  };
 
